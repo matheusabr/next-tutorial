0914dfa1cb0206bee303871798305b1b8294a5af
Matheus Abreu <mabreu@PDM-00870.lan>
2019-03-13 23:01:17 +0000

    task(Post): use dynamic page passing a router query

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import Layout from '../components/Layout'
+import Link from 'next/link'
 
-const Index = () => (
-  <Layout>
-    <p>Next.js tutorial</p>
-  </Layout>
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <p>{props.title}</p>
+    </Link>
+  </li>
 )
 
-export default Index
+export default () => (
+  <Layout>
+    <h1>Blog</h1>
+    <ul>
+      <PostLink title='Next.js Tutorial - Part 1' />
+      <PostLink title='Next.js Tutorial - Part 2' />
+      <PostLink title='Next.js Tutorial - Part 3' />
+    </ul>
+  </Layout>
+)
