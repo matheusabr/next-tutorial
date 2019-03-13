f2199639fe4fa343ee4d6d63e0669b2ba6f79ab9
Matheus Abreu <mabreu@PDM-00870.lan>
2019-03-13 00:24:21 +0000

    task(About): add About page and navigate to it

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import Link from 'next/link'
+
 const Index = () => (
   <div>
     <p>Next.js tutorial</p>
+    <Link href='/about'>
+      <a style={{ fontSize: 20 }}>Go to About Page</a>
+    </Link>
   </div>
 )
 
